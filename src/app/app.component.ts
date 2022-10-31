9241cda6aef892dfac9475b3a87a72dd4d832585
Rohitha Gourabathuni <rohithag@valuemomentum.biz>
2022-10-31 12:41:09 +0530

    propertybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PropertyBinding';
+  serverstatus: string= 'server not created';
+  allowNewServer = false;
+  createServer: boolean= false;
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer= true;
+    }, 3000)
+  }
+  onCreateServer(){
+ this.serverstatus= "new server has been created!";
+ this.createServer= true;
+  }
+  onUpdateServer(event: Event){
+    this.serverstatus= (<HTMLInputElement>event.target).value;
+  }
 }
